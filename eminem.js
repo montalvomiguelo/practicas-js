921a2ca629fe7335a88abb26492a90c1bf317471
imontalvomiguel <ubaldo.im@gmail.com>
2013-09-16 21:08:28 -0500

    slider v2 terminado

--- eminem.js
+++ eminem.js
@@ -1,44 +1,66 @@
 
 //Tamaño dinamico de los items de cada slide.
-for(var i=0; i<$('.slide').length; i++){ //itera cada slide
-	var $items = $('.slide')[i].children, //sus hijos ('a') item_slide
-		item;
-	$($items).each(function(index, domEle){ // iterar cada item_slide
-		item = $(domEle);
-		item.css({ //asignacion dinamica de colores y tamano
-			'background-color': item.data('bgc'),
-			'height': '100%',
-			'width': 100/$items.length+ '%'
+var indicador = 0, 
+	$slides = $('.slide'),
+	$siguiente = $('#right'),
+	$anterior = $('#left');
+	
+
+function coltam (){
+	for(var i=0; i<$('.slide').length; i++){ //itera cada slide
+		var $items = $('.slide')[i].children, //sus hijos ('a') item_slide
+			item;
+		$($items).each(function(index, domEle){ // iterar cada item_slide
+			item = $(domEle);
+			item.css({ //asignacion dinamica de colores y tamano
+				'background-color': item.data('bgc'),
+				'height': '100%',
+				'width': 100/$items.length+ '%'
+
+			});
+			console.log($(domEle).data('bgc'));
 
 		});
-		console.log($(domEle).data('bgc'));
-
-	});
-	console.log('slide posicion: '+i)
+		console.log('slide posicion: '+i);
+	}
 }
+coltam();
 
-
-var $slides = $('.slide');
 function tamano(){
 	$('#marco').css({ //tamaño del marco que contiene los slides
 		'height': $('#slider').width() * 0.56338028 + 'px', //altura relativa
 		'width': $('#slider').width() -90+ 'px'
 
 	});
-	/*
-	$slides.each(function(index, domEle){ //tamaño de cada slide
-		$(domEle).css({//tamaño de cada slide
-			'height': $('#slider').width() * 0.56338028 + 'px',
-			'width': $('#slider').width() -90+ 'px'
-		});
-	});
-*/
+	$('#slidesall').css('margin-left', -$('#slidesall').width()*indicador + 'px');
 }
 tamano();
+
 $(window).resize(tamano);
 
-$siguiente = $('#right');
-$siguiente.on('click',function(){
-	console.log('sguiente slide')
-})
+function mover(){
+	var contenedor = $('#slidesall');
+
+	if (indicador>=$slides.length){
+		indicador= 0;
+	}
+
+	if (indicador <0){
+		indicador = $slides.length-1;
+	}
+
+	console.log('indicador = '+indicador)
+	$(contenedor).animate({'margin-left': -$(contenedor).width()*indicador + 'px'});
+
+}
+
+$siguiente.on('click', function(){
+	indicador++;
+	mover();
+});
+
+$anterior.on('click', function(){
+	indicador--;
+	mover();
+});
 
